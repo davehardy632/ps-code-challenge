ddeb957965d4f3b50446895fe4edc419a248c0aa
David Hardy <davehardy632@gmail.com>
2020-02-04 13:27:02 -0700

    Adding a method to calculate the 50th percentile of a cafe dataset, in this case it is on cafes with an ls2 prefix.

--- app/models/street_cafe.rb
+++ app/models/street_cafe.rb
@@ -14,6 +14,16 @@ class StreetCafe < ApplicationRecord
     where.not("post_code LIKE 'LS1 %' OR post_code LIKE 'LS2 %'")
   end
 
+  def self.chair_numbers_percentile_50
+    sorted_number_of_chairs = pluck(:number_of_chairs).sort
+    index = (sorted_number_of_chairs.length * 0.5)
+    if index.floor == index
+      (sorted_number_of_chairs[index.to_i - 1] + sorted_number_of_chairs[index.to_i]) / 2.0
+    else
+      sorted_number_of_chairs[index.ceil - 1]
+    end
+  end
+
 
 
 end
