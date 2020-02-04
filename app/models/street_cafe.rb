e18378429817fc9cc324c715bd0a50787ee9fc1c
David Hardy <davehardy632@gmail.com>
2020-02-04 13:04:01 -0700

    Adding model methods to return cafes based on their post code prefix, ls1, ls1, and outlier post codes.

--- app/models/street_cafe.rb
+++ app/models/street_cafe.rb
@@ -4,4 +4,16 @@ class StreetCafe < ApplicationRecord
                         :post_code,
                         :number_of_chairs
 
+
+  def self.with_post_code_prefix(prefix)
+    add_whitespace = prefix + " "
+    where("post_code LIKE ?", "#{add_whitespace}%")
+  end
+
+  def self.post_code_outliers
+    where.not("post_code LIKE 'LS1 %' OR post_code LIKE 'LS2 %'")
+  end
+
+
+
 end
