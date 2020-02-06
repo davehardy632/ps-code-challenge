2cd2044c50a6fd29f07b4f718c3e27af37eb0497
David Hardy <davehardy632@gmail.com>
2020-02-05 17:13:11 -0700

    Updating test descriptions to be more explicit.

--- spec/models/street_cafe_spec.rb
+++ spec/models/street_cafe_spec.rb
@@ -26,76 +26,84 @@ describe StreetCafe, type: :model do
       let!(:categorized_ls1_cafes) {ls1_cafes.categorize_LS1_cafes}
       let!(:categorized_ls2_cafes) {ls2_cafes.categorize_LS2_cafes}
 
+    describe ".with_post_code_prefix" do
+      it "Returns street cafes when passed a valid prefix parameter" do
 
-    it "Returns street cafes when passed a valid prefix parameter" do
+        expect(StreetCafe.with_post_code_prefix('LS1').sort.first.name).to eq("Cafe 1")
+        expect(StreetCafe.with_post_code_prefix('LS1').sort.second.name).to eq("Cafe 2")
+        expect(StreetCafe.with_post_code_prefix('LS1').sort.third.name).to eq("Cafe 3")
 
-      expect(StreetCafe.with_post_code_prefix('LS1').sort.first.name).to eq("Cafe 1")
-      expect(StreetCafe.with_post_code_prefix('LS1').sort.second.name).to eq("Cafe 2")
-      expect(StreetCafe.with_post_code_prefix('LS1').sort.third.name).to eq("Cafe 3")
-
-      expect(StreetCafe.with_post_code_prefix('LS2').sort.first.name).to eq("Cafe 4")
-      expect(StreetCafe.with_post_code_prefix('LS2').sort.second.name).to eq("Cafe 5")
+        expect(StreetCafe.with_post_code_prefix('LS2').sort.first.name).to eq("Cafe 4")
+        expect(StreetCafe.with_post_code_prefix('LS2').sort.second.name).to eq("Cafe 5")
+      end
     end
 
+    describe ".categorize_LS1_cafes & .categorize_LS2_cafes" do
+      it "Categorizes street cafes with a post code prefix of LS1 and LS2" do
 
-    it "Categorizes street cafes with a post code prefix of LS1 and LS2" do
+        expect(ls1_cafes.categorize_LS1_cafes.sort.first.category).to eq("ls1 small")
+        expect(ls1_cafes.categorize_LS1_cafes.sort.second.category).to eq("ls1 medium")
+        expect(ls1_cafes.categorize_LS1_cafes.sort.third.category).to eq("ls1 large")
 
-      expect(ls1_cafes.categorize_LS1_cafes.sort.first.category).to eq("ls1 small")
-      expect(ls1_cafes.categorize_LS1_cafes.sort.second.category).to eq("ls1 medium")
-      expect(ls1_cafes.categorize_LS1_cafes.sort.third.category).to eq("ls1 large")
-
-      expect(ls2_cafes.categorize_LS2_cafes.sort.first.category).to eq("ls2 small")
-      expect(ls2_cafes.categorize_LS2_cafes.sort.second.category).to eq("ls2 large")
+        expect(ls2_cafes.categorize_LS2_cafes.sort.first.category).to eq("ls2 small")
+        expect(ls2_cafes.categorize_LS2_cafes.sort.second.category).to eq("ls2 large")
+      end
     end
 
+    describe ".categorize_post_code_outliers" do
+      it "Categorizes street cafes with a post code outlier 'NOT LS1 or LS2'" do
 
-    it "Categorizes street cafes with a post code outlier 'NOT LS1 or LS2'" do
+        expect(StreetCafe.categorize_post_code_outliers.sort.first.name).to eq("Cafe 6")
+        expect(StreetCafe.categorize_post_code_outliers.sort.first.category).to eq("other")
 
-      expect(StreetCafe.categorize_post_code_outliers.sort.first.name).to eq("Cafe 6")
-      expect(StreetCafe.categorize_post_code_outliers.sort.first.category).to eq("other")
-
-      expect(StreetCafe.categorize_post_code_outliers.sort.last.name).to eq("Cafe 7")
-      expect(StreetCafe.categorize_post_code_outliers.sort.last.category).to eq("other")
+        expect(StreetCafe.categorize_post_code_outliers.sort.last.name).to eq("Cafe 7")
+        expect(StreetCafe.categorize_post_code_outliers.sort.last.category).to eq("other")
+      end
     end
 
-    it "Calculates the 50th percentile of numbers of chairs on a street cafe dataset" do
+    describe ".chair_numbers_percentile_50" do
+      it "Calculates the 50th percentile of numbers of chairs on a street cafe dataset" do
 
 
-      expect(StreetCafe.with_post_code_prefix('LS2').chair_numbers_percentile_50).to eq(5.5)
+        expect(StreetCafe.with_post_code_prefix('LS2').chair_numbers_percentile_50).to eq(5.5)
 
-      StreetCafe.create(name: "Cafe 2", street_address: "13th st", post_code: "LS2 5CN", number_of_chairs: 7)
+        StreetCafe.create(name: "Cafe 2", street_address: "13th st", post_code: "LS2 5CN", number_of_chairs: 7)
 
-      expect(StreetCafe.with_post_code_prefix('LS2').chair_numbers_percentile_50).to eq(6)
+        expect(StreetCafe.with_post_code_prefix('LS2').chair_numbers_percentile_50).to eq(6)
+      end
     end
 
+    describe ".return_by_size" do
+      it "Returns street cafes by categorical size" do
 
-    it "Returns street cafes by categorical size" do
+        expect(StreetCafe.return_by_size("small").sort.first.name).to eq("Cafe 1")
+        expect(StreetCafe.return_by_size("small").sort.second.name).to eq("Cafe 4")
 
-      expect(StreetCafe.return_by_size("small").sort.first.name).to eq("Cafe 1")
-      expect(StreetCafe.return_by_size("small").sort.second.name).to eq("Cafe 4")
-
-      expect(StreetCafe.return_by_size("medium").sort.first.name).to eq("Cafe 2")
-      expect(StreetCafe.return_by_size("large").sort.first.name).to eq("Cafe 3")
-      expect(StreetCafe.return_by_size("large").sort.second.name).to eq("Cafe 5")
+        expect(StreetCafe.return_by_size("medium").sort.first.name).to eq("Cafe 2")
+        expect(StreetCafe.return_by_size("large").sort.first.name).to eq("Cafe 3")
+        expect(StreetCafe.return_by_size("large").sort.second.name).to eq("Cafe 5")
+      end
     end
 
+    describe ".concat_category_and_name" do
+      it "Concatenates the category to the beginning of the name on a street cafe dataset" do
 
-    it "Concatenates the category to the beginning of the name on a street cafe dataset" do
+        concatenated_ls1_cafes = ls1_cafes.concat_category_and_name
 
-      concatenated_ls1_cafes = ls1_cafes.concat_category_and_name
-
-      expect(concatenated_ls1_cafes.sort.first.name).to eq("ls1 small Cafe 1")
-      expect(concatenated_ls1_cafes.sort.second.name).to eq("ls1 medium Cafe 2")
-      expect(concatenated_ls1_cafes.sort.third.name).to eq("ls1 large Cafe 3")
+        expect(concatenated_ls1_cafes.sort.first.name).to eq("ls1 small Cafe 1")
+        expect(concatenated_ls1_cafes.sort.second.name).to eq("ls1 medium Cafe 2")
+        expect(concatenated_ls1_cafes.sort.third.name).to eq("ls1 large Cafe 3")
+      end
     end
 
+    describe ".to_csv" do
+      it "Formats a street cafe resource to an array, which can then be passed to a csv file" do
 
-    it "Formats a street cafe resource to an array, which can then be passed to a csv file" do
+        street_cafe = StreetCafe.first
 
-      street_cafe = StreetCafe.first
-
-      expect(street_cafe.to_csv.class).to eq(Array)
-      expect(street_cafe.to_csv).to eq([street_cafe.id, "Cafe 1", "12th st", "LS1 5BN", 3, "ls1 small"])
+        expect(street_cafe.to_csv.class).to eq(Array)
+        expect(street_cafe.to_csv).to eq([street_cafe.id, "Cafe 1", "12th st", "LS1 5BN", 3, "ls1 small"])
+      end
     end
   end
 end
